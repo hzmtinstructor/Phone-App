2bbec7cd76a4e8a5e14973be3392efd46e44ea49
hzmtinstructor <hzmtinstructor@gmail.com>
2026-04-17 10:06:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -51,13 +51,41 @@ CHECKLIST_ITEMS = [
 STATUS_OPTIONS = ["Not Started", "Complete", "Not Applicable"]
 
 # -------------------------
-# STATE
+# STATE INIT
 # -------------------------
 if "progress" not in st.session_state:
     st.session_state.progress = load_json(DATA_FILE)
 
 if "classes" not in st.session_state:
     st.session_state.classes = load_json(CLASS_FILE)
+
+# -------------------------
+# FORM RESET SYSTEM (SAFE)
+# -------------------------
+defaults = {
+    "form_name": "",
+    "form_course": "",
+    "form_osfm": "",
+    "form_instructor": "",
+    "form_start": datetime.today(),
+    "form_end": datetime.today(),
+    "form_location": "",
+    "form_t1": "",
+    "form_t2": "",
+    "form_comments": ""
+}
+
+if "reset_form" not in st.session_state:
+    st.session_state.reset_form = False
+
+if st.session_state.reset_form:
+    for k, v in defaults.items():
+        st.session_state[k] = v
+    st.session_state.reset_form = False
+
+for k, v in defaults.items():
+    if k not in st.session_state:
+        st.session_state[k] = v
 
 # -------------------------
 # HEADER
@@ -68,7 +96,10 @@ st.title("Stanly Community College Class Setup Dashboard")
 # SEARCH
 # -------------------------
 search = st.text_input("Search Classes").lower()
-class_names = [c for c in st.session_state.classes if search in c.lower()] if search else list(st.session_state.classes.keys())
+class_names = [
+    c for c in st.session_state.classes
+    if search in c.lower()
+] if search else list(st.session_state.classes.keys())
 
 # -------------------------
 # EXPORTS
@@ -95,12 +126,11 @@ def generate_audit_csv():
     return output.getvalue()
 
 st.subheader("Export Data")
-
 st.download_button("Download Class Report", generate_csv(), "class_report.csv")
 st.download_button("Download Audit History", generate_audit_csv(), "audit_history.csv")
 
 # -------------------------
-# PROGRESS DASHBOARD
+# DASHBOARD
 # -------------------------
 if class_names:
     st.subheader("Overall Class Progress")
@@ -133,41 +163,78 @@ if class_names:
 st.subheader("Add New Class")
 
 with st.form("add_class_form"):
-    name = st.text_input("Class Name")
-    course_code = st.text_input("Course Code")
-    osfm_portal = st.text_input("OSFM Portal")
-    instructor = st.text_input("Instructor")
+
+    st.text_input("Class Name", key="form_name")
+    st.text_input("Course Code", key="form_course")
+    st.text_input("OSFM Portal", key="form_osfm")
+    st.text_input("Instructor", key="form_instructor")
 
     col1, col2 = st.columns(2)
-    start_date = col1.date_input("Start Date")
-    end_date = col2.date_input("End Date")
+    col1.date_input("Start Date", key="form_start")
+    col2.date_input("End Date", key="form_end")
 
-    location = st.text_input("Location")
-    class_time1 = st.text_input("Class Times 1")
-    class_time2 = st.text_input("Class Times 2")
-    comments = st.text_area("Comments")
+    st.text_input("Location", key="form_location")
+    st.text_input("Class Times 1", key="form_t1")
+    st.text_input("Class Times 2", key="form_t2")
+    st.text_area("Comments", key="form_comments")
 
-    if st.form_submit_button("Add Class") and name:
-        st.session_state.classes[name] = {
-            "course_code": course_code,
-            "osfm_portal": osfm_portal,
-            "start_date": start_date.strftime("%Y-%m-%d"),
-            "end_date": end_date.strftime("%Y-%m-%d"),
-            "instructor": instructor,
-            "location": location,
-            "class_time1": class_time1,
-            "class_time2": class_time2,
-            "comments": comments,
-            "visitation_date": ""
+    col_add, col_clear = st.columns(2)
+    submitted = col_add.form_submit_button("Add Class")
+    cleared = col_clear.form_submit_button("Clear Fields")
+
+# Handle actions AFTER form
+if submitted and st.session_state.form_name:
+
+    name = st.session_state.form_name
+
+    st.session_state.classes[name] = {
+        "course_code": st.session_state.form_course,
+        "osfm_portal": st.session_state.form_osfm,
+        "start_date": st.session_state.form_start.strftime("%Y-%m-%d"),
+        "end_date": st.session_state.form_end.strftime("%Y-%m-%d"),
+        "instructor": st.session_state.form_instructor,
+        "location": st.session_state.form_location,
+        "class_time1": st.session_state.form_t1,
+        "class_time2": st.session_state.form_t2,
+        "comments": st.session_state.form_comments,
+        "visitation_date": ""
+    }
+
+    log_change(name, "Class Created", "", "New Class")
+    save_json(CLASS_FILE, st.session_state.classes)
+
+    st.session_state.reset_form = True
+    st.success(f"Added class: {name}")
+    st.rerun()
+
+if cleared:
+    st.session_state.reset_form = True
+    st.rerun()
+
+# -------------------------
+# DELETE
+# -------------------------
+st.subheader("Delete Classes")
+
+if st.session_state.classes:
+    to_delete = st.multiselect("Select classes", list(st.session_state.classes.keys()))
+
+    if st.button("Delete Selected"):
+        for cls in to_delete:
+            log_change(cls, "Class Deleted", "Exists", "Deleted")
+            st.session_state.classes.pop(cls, None)
+
+        st.session_state.progress = {
+            k:v for k,v in st.session_state.progress.items()
+            if not any(k.startswith(f"{cls}_") for cls in to_delete)
         }
 
-        log_change(name, "Class Created", "", "New Class")
-
         save_json(CLASS_FILE, st.session_state.classes)
+        save_json(DATA_FILE, st.session_state.progress)
         st.rerun()
 
 # -------------------------
-# CLASS DETAILS + CHECKLIST (FULLY RESTORED)
+# CLASS DETAILS + CHECKLIST (FULL RESTORE)
 # -------------------------
 if class_names:
     st.subheader("Class Checklists")
@@ -185,8 +252,19 @@ if class_names:
             new_inst = st.text_input("Instructor", data.get("instructor",""), key=f"{cls}_inst")
 
             col1, col2 = st.columns(2)
-            new_start = col1.date_input("Start Date", datetime.today(), key=f"{cls}_start")
-            new_end = col2.date_input("End Date", datetime.today(), key=f"{cls}_end")
+
+            try:
+                start_val = datetime.strptime(data.get("start_date",""), "%Y-%m-%d")
+            except:
+                start_val = datetime.today()
+
+            try:
+                end_val = datetime.strptime(data.get("end_date",""), "%Y-%m-%d")
+            except:
+                end_val = datetime.today()
+
+            new_start = col1.date_input("Start Date", start_val, key=f"{cls}_start")
+            new_end = col2.date_input("End Date", end_val, key=f"{cls}_end")
 
             new_loc = st.text_input("Location", data.get("location",""), key=f"{cls}_loc")
             new_t1 = st.text_input("Class Times 1", data.get("class_time1",""), key=f"{cls}_t1")
@@ -203,9 +281,7 @@ if class_names:
 
                 old_status = st.session_state.progress[key]["status"]
 
-                new_status = st.selectbox(
-                    item,
-                    STATUS_OPTIONS,
+                new_status = st.selectbox(item, STATUS_OPTIONS,
                     index=STATUS_OPTIONS.index(old_status),
                     key=f"{key}_status"
                 )
@@ -220,13 +296,14 @@ if class_names:
                         visit_date = st.date_input("Visitation Date", datetime.today(), key=f"{cls}_visit")
                         st.session_state.classes[cls]["visitation_date"] = visit_date.strftime("%Y-%m-%d")
 
-            if st.button(f"Save {cls}"):
+            if st.button(f"Save {cls}", key=f"{cls}_save"):
 
-                # rename logic
-                if new_name != cls:
-                    st.session_state.classes[new_name] = st.session_state.classes.pop(cls)
+                final_name = new_name.strip()
 
-                st.session_state.classes[new_name].update({
+                if final_name and final_name != cls:
+                    st.session_state.classes[final_name] = st.session_state.classes.pop(cls)
+
+                st.session_state.classes[final_name].update({
                     "course_code": new_course,
                     "osfm_portal": new_osfm,
                     "start_date": new_start.strftime("%Y-%m-%d"),
@@ -241,5 +318,10 @@ if class_names:
                 save_json(CLASS_FILE, st.session_state.classes)
                 save_json(DATA_FILE, st.session_state.progress)
 
-                st.success(f"Updated {new_name}")
+                st.success(f"Updated {final_name}")
                 st.rerun()
+
+    save_json(DATA_FILE, st.session_state.progress)
+
+else:
+    st.info("No classes found")
